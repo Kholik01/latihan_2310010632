debaf7e684bd71a8fdc4f2b5c515223322acc07a
kholik01 <abdi200803@gmail.com>
2025-06-10 11:09:12 +0700

    Melengkapi method matematika

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        
+        Matematika abdi = new Matematika(3,2);
+        
+        System.out.println("hasil penjumlahan: "+abdi.setPenjumlahan());
+        System.out.println("hasil pengurangan: "+abdi.setPengurangan());
+        System.out.println("hasil perkalian: "+abdi.setPerkalian());
+        System.out.println("hasil pembagian: "+abdi.setPembagian());
+    }
 }
